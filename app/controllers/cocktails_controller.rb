5a6dc5d9cd71da473e19fd10f7d8055cbf4b11ef
acortes8 <46253800+acortes8@users.noreply.github.com>
2024-03-03 22:21:19 -0600

    Change controller to exclude the newly added search and multi_search from auth restriction

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,5 +1,5 @@
 class CocktailsController < ApplicationController
-  http_basic_authenticate_with name: "brevity", password: "CT9d43", except: [:index, :show]
+  http_basic_authenticate_with name: "brevity", password: "CT9d43", except: [:index, :show, :search, :multi_search]
 
   def index
     @cocktails = Cocktail.all
