a4185b476a2588241b79d0d5e28ae276c2ba1d53
Alejandro Cortes <46253800+acortes8@users.noreply.github.com>
2024-09-20 12:18:00 -0500

    Update to config/importmap file to add BootstrapJS
    "Hamburger" drop down in navbar was not working on mobile version of app

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,6 +7,7 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "stimulus-rails-nested-form", to: "https://ga.jspm.io/npm:stimulus-rails-nested-form@4.1.0/dist/stimulus-rails-nested-form.mjs"
 
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
 pin "ingredients", preload: true
